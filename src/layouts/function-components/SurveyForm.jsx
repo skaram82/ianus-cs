971374bf9e194c4621c39c1cff8a1f1c346f4e8a
skaram82 <stelios.karamanidis@gmail.com>
2024-02-01 13:04:20 +0200

    Fix test form

--- src/layouts/function-components/SurveyForm.jsx
+++ src/layouts/function-components/SurveyForm.jsx
@@ -117,12 +117,12 @@ function SurveyForm() {
         </li>
     </ol>
     {/* <form name="survey" data-netlify = "true" netlify-honeypot = "bot-field" action="/success" method="POST" hidden> */}
-    <form name="survey" data-netlify = "true" netlify-honeypot = "bot-field" hidden>
+    {/* <form name="survey" data-netlify = "true" netlify-honeypot = "bot-field" hidden>
       <input id="name" type="text" name="name" value={formData.name} readOnly />
       <input id="email" type="email" name="email" value={formData.email} readOnly />
       <input id="info" type="text" name="info" value={formData.info} readOnly />
       <input id="feedback" type="text" name="feedback" value={formData.feedback} readOnly />
-    </form>
+    </form> */}
     <form
         // data-netlify = "true"
         // netlify-honeypot = "bot-field"
